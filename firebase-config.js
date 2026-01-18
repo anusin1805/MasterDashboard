cf7251cf368f6e8d72027a475edade672fc77a54
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-01-18 19:41:46 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC1hVvK7Yd3qB1Ud3mbIKd_edq4XmH-PVI",
+    authDomain: "brave-calling-398109.firebaseapp.com",
+    projectId: "brave-calling-398109",
+    storageBucket: "brave-calling-398109.firebasestorage.app",
+    messagingSenderId: "118774845365",
+    appId: "1:118774845365:web:99841a1b01f387a48ed3fc"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
